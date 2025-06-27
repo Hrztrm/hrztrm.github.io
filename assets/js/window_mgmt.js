011ba575e11915cac613f0df144b6a6e428fdf82
Hashing Tuna <hrztrmz@gmail.com>
2025-06-27 12:15:24 +0800

    changein background colour

--- assets/js/window_mgmt.js
+++ assets/js/window_mgmt.js
@@ -3,11 +3,18 @@ document.body.addEventListener('click', function (event) {
     const clickedInPost = event.target.closest('#post-box');
     const clickedInPopup = event.target.closest('#neko-popup');
 
-    if (clickedInPopup) {
-        console.log('Inside popup');
+    if (clickedInNav) {
+        document.getElementById('nav-box').style.background = '#00007f';
+        document.getElementById('post-box').style.display = '#808080';
+        document.getElementById('neko-popup').style.display = '#808080';
     } else if (clickedInPost) {
-        console.log('Inside post');
-    } else if (clickedInNav) {
-        console.log('Inside nav');
+        document.getElementById('nav-box').style.background = '#808080';
+        document.getElementById('post-box').style.display = '#00007f';
+        document.getElementById('neko-popup').style.display = '#808080';
+    } else if (clickedInPopup) {
+        document.getElementById('nav-box').style.background = '#808080';
+        document.getElementById('post-box').style.display = '#808080';
+        document.getElementById('neko-popup').style.display = '#00007f';
+        console.log('Inside popup');
     }
 });
