db56173cf2adb7f34fed97fd621a1ad8d89918ea
Hashing Tuna <hrztrmz@gmail.com>
2025-06-27 12:19:08 +0800

    update

--- assets/js/window_mgmt.js
+++ assets/js/window_mgmt.js
@@ -5,16 +5,16 @@ document.body.addEventListener('click', function (event) {
 
     if (clickedInNav) {
         document.getElementById('nav-box').style.background = '#00007f';
-        document.getElementById('post-box').style.display = '#808080';
-        document.getElementById('neko-popup').style.display = '#808080';
+        document.getElementById('post-box').style.background = '#808080';
+        document.getElementById('neko-popup').style.background = '#808080';
     } else if (clickedInPost) {
         document.getElementById('nav-box').style.background = '#808080';
-        document.getElementById('post-box').style.display = '#00007f';
-        document.getElementById('neko-popup').style.display = '#808080';
+        document.getElementById('post-box').style.background = '#00007f';
+        document.getElementById('neko-popup').style.background = '#808080';
     } else if (clickedInPopup) {
         document.getElementById('nav-box').style.background = '#808080';
-        document.getElementById('post-box').style.display = '#808080';
-        document.getElementById('neko-popup').style.display = '#00007f';
+        document.getElementById('post-box').style.background = '#808080';
+        document.getElementById('neko-popup').style.background = '#00007f';
         console.log('Inside popup');
     }
 });
